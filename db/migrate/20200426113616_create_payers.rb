b2f6369e110ab0b171bd94fd13ab55c4eee49793
Anoop R <ranoop@reflektive.com>
2020-04-27 06:07:39 +0530

    Changed DB defaults

--- db/migrate/20200426113616_create_payers.rb
+++ db/migrate/20200426113616_create_payers.rb
@@ -2,7 +2,7 @@ class CreatePayers < ActiveRecord::Migration[5.0]
   def change
     create_table :payers do |t|
       t.string :full_name, null: false
-      t.text :address, null: false
+      t.text :address
 
       t.timestamps
     end
--- db/migrate/20200426122357_create_bills.rb
+++ db/migrate/20200426122357_create_bills.rb
@@ -1,11 +1,11 @@
 class CreateBills < ActiveRecord::Migration[5.0]
   def change
     create_table :bills do |t|
-      t.integer :payer_id, null: false
+      t.integer :payer_id,       null: false
       t.date :bill_date
-      t.float :bill_amount
+      t.float :bill_amount,      default: 0.0
       t.string :bill_status
-      t.float :payment_amount
+      t.float :payment_amount,   default: 0.0
       t.date :payment_date
 
       t.timestamps
